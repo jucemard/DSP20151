7f295e548bfa0b67c5a8db000d0925b8da0c15e9
Bruno Pachulski <yakko.acme@gmail.com>
2015-02-09 20:29:19 -0200

    Alterando mensagem inicial

--- DSP20151/app/src/main/java/br/grupointegrado/dspapp01/MainActivity.java
+++ DSP20151/app/src/main/java/br/grupointegrado/dspapp01/MainActivity.java
@@ -3,6 +3,7 @@ package br.grupointegrado.dspapp01;
 import android.app.Activity;
 import android.support.v7.app.ActionBarActivity;
 import android.os.Bundle;
+import android.widget.TextView;
 
 
 public class MainActivity extends Activity {
@@ -11,7 +12,10 @@ public class MainActivity extends Activity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-        
+
+        TextView tvHelloWorld = (TextView) findViewById(R.id.tvHelloWorld);
+        tvHelloWorld.setText("Olá mundo !! =D");
+
     }
 
 }
