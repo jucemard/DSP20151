d64346badccdd32e91f6a7a44ccac3ef54ba0b3d
Bruno Pachulski <yakko.acme@gmail.com>
2015-02-09 20:19:39 -0200

    Primeiro projeto

---
+++ DSP20151/app/src/main/java/br/grupointegrado/dspapp01/MainActivity.java
@@ -0,0 +1,17 @@
+package br.grupointegrado.dspapp01;
+
+import android.app.Activity;
+import android.support.v7.app.ActionBarActivity;
+import android.os.Bundle;
+
+
+public class MainActivity extends Activity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        
+    }
+
+}
