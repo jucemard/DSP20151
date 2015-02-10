c26b96b59f05594f3520c11274ee87fc93c52b0b
Bruno Pachulski <yakko.acme@gmail.com>
2015-02-10 21:22:08 -0200

    Final da aula 09/02/15

--- DSP20151/app/src/main/java/br/grupointegrado/dspapp01/MainActivity.java
+++ DSP20151/app/src/main/java/br/grupointegrado/dspapp01/MainActivity.java
@@ -3,6 +3,8 @@ package br.grupointegrado.dspapp01;
 import android.app.Activity;
 import android.support.v7.app.ActionBarActivity;
 import android.os.Bundle;
+import android.util.Log;
+import android.view.View;
 import android.widget.TextView;
 
 
@@ -14,7 +16,18 @@ public class MainActivity extends Activity {
         setContentView(R.layout.activity_main);
 
         TextView tvHelloWorld = (TextView) findViewById(R.id.tvHelloWorld);
-        tvHelloWorld.setText("Olá mundo !! =D");
+        tvHelloWorld.setText("Olá mundo !! =D ... ");
+
+        tvHelloWorld.setOnClickListener(new View.OnClickListener() {
+
+            @Override
+            public void onClick(View v) {
+
+                Log.e("MSG", "Teste de Botão !! ");
+
+            }
+
+        });
 
     }
 
