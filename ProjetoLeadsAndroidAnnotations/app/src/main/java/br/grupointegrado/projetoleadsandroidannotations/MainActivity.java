77666e05557f6677bbaeee412c1b36ef75571300
Bruno Pachulski <yakko.acme@gmail.com>
2015-05-05 09:56:17 -0300

    Correções no AndroidAnnotations

--- ProjetoLeadsAndroidAnnotations/app/src/main/java/br/grupointegrado/projetoleadsandroidannotations/MainActivity.java
+++ ProjetoLeadsAndroidAnnotations/app/src/main/java/br/grupointegrado/projetoleadsandroidannotations/MainActivity.java
@@ -5,11 +5,18 @@ import android.support.v7.app.ActionBarActivity;
 import android.os.Bundle;
 import android.view.Menu;
 import android.view.MenuItem;
+import android.widget.Toast;
 
+import org.androidannotations.annotations.Click;
 import org.androidannotations.annotations.EActivity;
 import org.androidannotations.annotations.NoTitle;
 
 @EActivity(R.layout.activity_main)
 public class MainActivity extends Activity {
+
+    @Click(R.id.lbTest)
+    public void clickLb () {
+        Toast.makeText(this, "Funcionando", Toast.LENGTH_LONG).show();
+    }
 
 }
